e2c42fd03e78e4d33e9367438a1250c2d740da3e
Himalchals4038 <rakhi_mondal@outlook.com>
2026-06-11 23:37:08 +0530

    11 pm 11/06/26

--- Apna College/04. DOM/script.js
+++ Apna College/04. DOM/script.js
@@ -33,3 +33,44 @@ for (let item of listItems){
     item.style.fontSize = "20px";
     item.style.fontFamily = "Comic Sans MS";
 }
+
+//Returns first element instance
+let para = document.querySelector("p");
+// console.log(para);
+para.style.color = "rgb(195, 0, 255)";
+para.style.fontSize = "12px";
+para.style.fontFamily = "verdana";
+
+//Returns all elements instances
+let list = document.querySelectorAll("li");
+// console.log(list);
+for (let item of list){
+    item.style.color = "rgb(117, 51, 7)";
+    item.style.backgroundColor = "rgb(92, 231, 79)";
+    item.style.fontSize = "17px";
+    item.style.fontFamily = "trebuchet ms";
+}
+
+let project = document.querySelector("#clone-website");
+// console.log(project);
+project.style.display = "flex";
+project.style.flexDirection = "column";
+project.style.justifyContent = "center";
+project.style.alignItems = "center";
+project.style.listStyleType = "none";
+project.style.paddingBottom = "20px";
+
+let amazonClone = document.querySelector("#Amazon");
+// console.log(amazonClone);
+amazonClone.style.padding = "10px";
+amazonClone.style.backgroundColor = "rgb(225, 226, 132)";
+amazonClone.style.color = "rgb(21, 85, 8)";
+amazonClone.style.padding = "10px";
+
+let flipkartClone = document.querySelector("#Flipkart");
+flipkartClone.innerText = "Instamart";
+
+// console.dir(document.body.children[2]);
+document.body.children[2].style.color = "rgb(12, 201, 185)";
+// console.dir(document.body.firstChild.nextElementSibling);
+document.body.firstChild.nextElementSibling.style.color = "rgb(94, 106, 221)";
